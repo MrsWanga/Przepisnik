174cebd28081201129e3c2ffbf51540451fad485
Patrycja Wanga <patrycja.wanga@gmail.com>
2022-06-07 17:54:40 +0000

    Merged in feature/controller (pull request #3)
    Feature/controller
    
    * tablice stworzono w konsoli i stworzono domyslna dlugosc stringa
    
    * Stworzenie relacji jeden do wielu
    
    * Stworzenie controllera i zmapowanie
    
    * Merge branch 'develop' into feature/controller
    
    # Conflicts:
    #	app/Models/Ingredient.php
    #	app/Models/Recipe.php
    
    * fix routing
    
    
    Approved-by: Hubert Wanga

---
+++ app/Http/Controllers/RecipeController.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class RecipeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        dd("test index");
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        dd("test create");
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        dd("test store");
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        dd("test show");
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        dd("test edit");
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        dd("test update");
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        dd("test destroy");
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\RecipeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +16,25 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::name('recipes.')->prefix('recipes')->group(function () {
+    Route::get('', [RecipeController::class, 'index'])
+        ->name('index');
+    Route::get('create', [RecipeController::class, 'create'])
+        ->name('create');
+    Route::post('', [RecipeController::class, 'store'])
+        ->name('store');
+    Route::get('{id}', [RecipeController::class, 'show'])
+        ->name('show')
+        ->where('id', '[0-9]+');
+    Route::get('{id}/edit', [RecipeController::class, 'edit'])
+        ->name('edit')
+        ->where('id', '[0-9]+');
+    Route::patch('{id}', [RecipeController::class, 'update'])
+        ->name('update')
+        ->where('id', '[0-9]+');
+    Route::get('{id}/delete', [RecipeController::class, 'destroy'])
+        ->name('destroy')
+        ->where('id', '[0-9]+');
 });
